d3dc4febd984a536a1511c5e10d43d162ac547a8
AlbeUni <alberto.reginato.1@studenti.unipd.it>
2025-05-05 16:49:43 +0200

    Implementato funzione RomanPrinter, close #3

--- roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -6,12 +6,57 @@
 package it.unipd.mtss;
 
 public class RomanPrinter {
-    public static String print(int num){
-            return printAsciiArt(IntegerToRoman.convert(num));
+
+    private static final String SYMBOLS = "MDCLXVI";
+
+    public static String print(int num) {
+        return printAsciiArt(IntegerToRoman.convert(num));
+    }
+
+    private static String printAsciiArt(String romanNumber) {
+        StringBuilder asciiArt = new StringBuilder();
+
+        String[][] asciiLetters = {
+            { 
+              "  __  __ ", "  _____  ", "   _____ ", "  _      ",
+              " __   __", " __      __", "  _____ "
+            },
+            {
+              " |  \\/  |", " |  __ \\ ", "  / ____|", " | |     ",
+              " \\ \\ / /", " \\ \\    / /", " |_   _|"
+            },
+            {
+              " | \\  / |", " | |  | |", " | |     ", " | |     ",
+              "  \\ V / ", "  \\ \\  / /", "   | |  "
+            },
+            { 
+              " | |\\/| |", " | |  | |", " | |     ", " | |     ",
+              "   > <  ", "   \\ \\/ /", "   | |  "
+            },
+            { 
+              " | |  | |", " | |__| |", " | |____ ", " | |____ ",
+              "  / . \\ ", "    \\  /", "  _| |_ "
+            },
+            { 
+              " |_|  |_|", " |_____/ ", "  \\_____|", " |______|",
+              " /_/ \\_\\", "     \\/  ", " |_____|"
+            }
+        };
+
+        for (int row = 0; row < asciiLetters.length; row++) {
+            for (char c : romanNumber.toCharArray()) {
+                int idx = SYMBOLS.indexOf(c);
+                if (idx >= 0) {
+                    asciiArt.append(asciiLetters[row][idx])
+                            .append("  "); 
+                } else {
+                    int width = asciiLetters[row][0].length();
+                    asciiArt.append(" ".repeat(width)).append("  ");
+                }
+            }
+            asciiArt.append(System.lineSeparator());
         }
 
-        private static String printAsciiArt(String romanNumber){
-            //TODO
-            return null;
-        }
+        return asciiArt.toString();
+    }
 }
