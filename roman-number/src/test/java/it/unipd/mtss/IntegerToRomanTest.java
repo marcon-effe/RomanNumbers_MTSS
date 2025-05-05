c0d76360e3c452841d97b66d578ffc715dcc2dbc
Francesco <framar514@gmail.com>
2025-05-05 22:26:59 +0200

    Aggiunta dei test per i numeri fino a 3, close #6

--- roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -5,6 +5,14 @@
 
 package it.unipd.mtss;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class IntegerToRomanTest {
-    
+    @Test
+    public void testConvert() {
+        assertEquals("I", IntegerToRoman.convert(1));
+        assertEquals("II", IntegerToRoman.convert(2));
+        assertEquals("III", IntegerToRoman.convert(3));
+    }
 }
